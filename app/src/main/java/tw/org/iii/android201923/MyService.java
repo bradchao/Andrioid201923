ddf3cdcfdfc11c4f4bb01d94dae8cb37ee1da1d5
brad <brad@brad.tw>
2019-12-27 21:16:06 +0800

    2

--- app/src/main/java/tw/org/iii/android201923/MyService.java
+++ app/src/main/java/tw/org/iii/android201923/MyService.java
@@ -8,6 +8,9 @@ import android.util.Log;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Map;
+import java.util.Set;
+
 public class MyService extends FirebaseMessagingService {
     public MyService() {
     }
@@ -30,13 +33,10 @@ public class MyService extends FirebaseMessagingService {
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.v("brad", "Message data payload: " + remoteMessage.getData());
-
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-                //scheduleJob();
-            } else {
-                // Handle message within 10 seconds
-                //handleNow();
+            Map<String,String> data = remoteMessage.getData();
+            Set<String> keys = data.keySet();
+            for (String key :  keys){
+                Log.v("brad", key + "=>" + data.get(key));
             }
 
         }
@@ -49,5 +49,16 @@ public class MyService extends FirebaseMessagingService {
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
     }
+
+    @Override
+    public void onNewToken(String token) {
+        Log.v("brad", "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(token);
+    }
+
 
 }
